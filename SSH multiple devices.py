0649bcb86e882a23c43a09f685c5d7d889da1ea0
Keshawn Tindal <83261223+KTindal@users.noreply.github.com>
2022-08-04 17:00:02 -0600

    Added script for remote command execution to multiple devices

---
+++ SSH multiple devices.py
@@ -0,0 +1,24 @@
+## SSH to multiple hosts from a text file host.txt ##
+from netmiko import ConnectHandler
+
+with open('host.txt') as switches:
+    for IP in switches:
+        Switch = {
+            'device_type': 'cisco_ios',
+            'ip': IP,
+            'username': 'admin',
+            'password': 'cisco'
+        }
+
+        net_connect = ConnectHandler(**Switch)
+
+        print('Connecting to ' + IP)
+        print('-'*79)
+        output = net_connect.send_command('sh ip int brief')
+        print(output)
+        print()
+        print('-'*79)
+
+# Close Connection
+net_connect.disconnect()
+
